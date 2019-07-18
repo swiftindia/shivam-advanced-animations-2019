94e1a1f8e536445e616179aa6906759403f7159e
Shivam <>
2019-07-18 17:43:37 +0530

    Final commit for Layers

--- Layer Animation/Final/BasicViewAnimations/BasicViewAnimations/ViewController.swift
+++ Layer Animation/Final/BasicViewAnimations/BasicViewAnimations/ViewController.swift
@@ -22,21 +22,22 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.balloon.alpha = 1
+        cloud1.layer.shadowOpacity = 0.7
+        cloud1.layer.shadowOffset = CGSize(width: 5, height: 5)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         animateCloud(cloud1)
         animateCloud(cloud2)
-        
+        animateBalloon()
     }
 
     func animateCloud(_ cloud: UIImageView){
         let distance = view.frame.size.width - cloud.frame.origin.x
         let cloudSpeed = 10.0 / view.frame.size.width
         let time = distance * cloudSpeed
-        UIView.animate(withDuration: TimeInterval(time),  delay: 0, options: .curveLinear, animations: {
+        UIView.animate(withDuration: TimeInterval(time), delay: 0, options: .curveLinear, animations: {
             cloud.frame.origin = CGPoint(x: self.view.frame.width, y: cloud.frame.origin.y)
         }) { _ in
             cloud.frame.origin = CGPoint(x: -cloud.frame.size.width, y: cloud.frame.origin.y)
@@ -44,7 +45,15 @@ class ViewController: UIViewController {
         }
     }
     
-    
-   
+    func animateBalloon(){
+        let fly = CABasicAnimation(keyPath: "position.y")
+        fly.fromValue = balloon.layer.position.y
+        fly.toValue = 100.0
+        fly.duration = 5.0
+        fly.beginTime = CACurrentMediaTime() + 1.0
+        fly.fillMode = .backwards
+        balloon.layer.add(fly, forKey: nil)
+        balloon.layer.position.y = 100
+    }
 }
 
