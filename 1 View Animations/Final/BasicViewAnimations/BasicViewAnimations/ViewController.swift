4f06d27b58a5f481eb8a25a23556a4af0f8386d6
Shivam <>
2019-07-26 15:12:26 +0530

    updated keyframe

--- 1 View Animations/Final/BasicViewAnimations/BasicViewAnimations/ViewController.swift
+++ 1 View Animations/Final/BasicViewAnimations/BasicViewAnimations/ViewController.swift
@@ -17,81 +17,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var balloon: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-//        self.cloud1.center.x -= self.view.bounds.width
-//        self.cloud2.center.x -= self.view.bounds.width
-//        self.cloud1.alpha = 0
-//        self.cloud2.alpha = 0
-        self.balloon.alpha = 1
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         animateCloud(cloud1)
         animateCloud(cloud2)
-        floatTheBalloonWithKeyFrame()
-        //floatTheBalloon()
+        flyBalloonWithKeyframe()
     }
     
-    //Basic Animation
-    func floatCloud(){
-        UIView.animate(withDuration: 3) {
-            self.cloud1.center.x += self.view.bounds.width
-            self.cloud2.center.x += self.view.bounds.width
-        }
-    }
-
-    func floatCloudWithDelay(){
-        UIView.animate(withDuration: 3, delay: 0, options: [], animations: {
-            self.cloud1.center.x += self.view.bounds.width
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 3, delay: 1, options: [], animations: {
-            self.cloud2.center.x += self.view.bounds.width
-
-        }, completion: nil)
-    }
-    
-    func floatCloudWithAnimationOptions(){
-        UIView.animate(withDuration: 1, delay: 1, options: [], animations: {
-            self.cloud1.center.x = 250
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 1, delay: 1, options: [.repeat,.autoreverse, .curveEaseInOut], animations: {
-            self.cloud2.center.x = 250
-            
-        }, completion: nil)
-    }
-    
-    
-    func floatCloudWithOtherAnimatableProperties(){
-        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseInOut, animations: {
-            self.cloud1.center.x += self.view.bounds.width
-            self.cloud2.center.x += self.view.bounds.width
-            self.cloud1.alpha = 1
-            self.cloud2.alpha = 0.7
-        }, completion: nil)
-    }
-
-    func animateCloud(_ cloud: UIImageView){
-        let distance = view.frame.size.width - cloud.frame.origin.x
-        let cloudSpeed = 10.0 / view.frame.size.width
-        let time = distance * cloudSpeed
-        UIView.animate(withDuration: TimeInterval(time),  delay: 0, options: .curveLinear, animations: {
-            cloud.frame.origin = CGPoint(x: self.view.frame.width, y: cloud.frame.origin.y)
-        }) { _ in
-            cloud.frame.origin = CGPoint(x: -cloud.frame.size.width, y: cloud.frame.origin.y)
+    func animateCloud(_ cloud: UIView){
+        let distance = view.bounds.width - cloud.frame.origin.x
+        let speed = view.bounds.width / 10
+        let duration = distance / speed
+        UIView.animate(withDuration: TimeInterval(duration), delay: 0, options: [ .curveLinear], animations: {
+            cloud.center.x += distance
+        }) { (_) in
+            cloud.center = CGPoint(x: -cloud.bounds.midX, y: cloud.center.y)
             self.animateCloud(cloud)
         }
     }
     
-    
-    func floatTheBalloon(){
+    func flyTheBalloon(){
         let center = balloon.center
+        
         UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut], animations: {
             self.balloon.center.x -= 150
             self.balloon.center.y -= 200
@@ -104,7 +57,7 @@ class ViewController: UIViewController {
                     self.balloon.center.x = self.view.center.x
                     self.balloon.center.y = 150
                 }, completion: { _ in
-                    UIView.animate(withDuration: 1, delay: 15, options: [.curveEaseInOut], animations: {
+                    UIView.animate(withDuration: 1, delay: 6, options: [.curveEaseInOut], animations: {
                         self.balloon.center = center
                     }, completion: nil)
                 })
@@ -112,32 +65,33 @@ class ViewController: UIViewController {
         })
     }
     
-    func floatTheBalloonWithKeyFrame(){
+    func flyBalloonWithKeyframe(){
         let center = balloon.center
-        UIView.animateKeyframes(withDuration: 10, delay: 0, options: [], animations: {
-            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.1, animations: {
+        
+        UIView.animateKeyframes(withDuration: 10, delay: 0, options: [.calculationModeLinear], animations: {
+            
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/10, animations: {
                 self.balloon.center.x -= 150
                 self.balloon.center.y -= 200
-                self.balloon.transform = CGAffineTransform(rotationAngle: -.pi/8)
             })
             
-            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.25, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 1/10, relativeDuration: 1/10, animations: {
                 self.balloon.center.x += 300
                 self.balloon.center.y -= 200
-                self.balloon.transform = CGAffineTransform(rotationAngle: .pi/8)
-            })
-
-            UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.3, animations: {
-                self.balloon.center.x = self.view.center.x
-                self.balloon.center.y = 150
-                self.balloon.transform = CGAffineTransform(rotationAngle: 0)
             })
             
-            UIView.addKeyframe(withRelativeStartTime: 0.95, relativeDuration: 0.05, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 2/10, relativeDuration: 1/10, animations: {
+                self.balloon.center.x = self.view.center.x
+                self.balloon.center.y = 150
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 9/10, relativeDuration: 1/10, animations: {
                 self.balloon.center = center
             })
             
-        }, completion: nil)
+        }) { (_) in
+            
+        }
     }
 }
 
--- 1 View Animations/Starter/BasicViewAnimations/BasicViewAnimations/ViewController.swift
+++ 1 View Animations/Starter/BasicViewAnimations/BasicViewAnimations/ViewController.swift
@@ -17,7 +17,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var balloon: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        balloon.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    func flyTheBalloon(){
+        let center = balloon.center
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut], animations: {
+            self.balloon.center.x -= 150
+            self.balloon.center.y -= 200
+        }, completion: { _ in
+            UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut], animations: {
+                self.balloon.center.x += 300
+                self.balloon.center.y -= 200
+            }, completion: { _ in
+                UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut], animations: {
+                    self.balloon.center.x = self.view.center.x
+                    self.balloon.center.y = 150
+                }, completion: { _ in
+                    UIView.animate(withDuration: 1, delay: 6, options: [.curveEaseInOut], animations: {
+                        self.balloon.center = center
+                    }, completion: nil)
+                })
+            })
+        })
+    }*/
 }
 
